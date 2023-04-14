075fcaf7f769a0f482fdb02eb7be538fecb2540e
ik3 <aybikeboran7140@gmail.com>
2023-04-15 00:58:43 +0300

    Web Api Post & Get operations were added.

---
+++ WebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Questions.Commands.Add;
+using Application.Features.Questions.Queries.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ApiControllerBase
+    {
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddAsync(QuestionAddCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await Mediator.Send(new QuestionGetAllQuery(id, false)));
+        }
+
+    }
+}
