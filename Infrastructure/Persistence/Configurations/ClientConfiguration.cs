145010343c95c061b16274d971af3d2b5ca827d6
ik3 <aybikeboran7140@gmail.com>
2023-04-14 03:11:13 +0300

    Add Configurations & Relationships

---
+++ Infrastructure/Persistence/Configurations/ClientConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class ClientConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            // Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+                       
+            // Relationships
+            builder.HasMany<Comment>(x => x.Comments)
+                .WithOne(x => x.Client)
+                .HasForeignKey(x => x.ClientId);
+            // Relationships
+            builder.HasMany<Question>(x => x.Questions)
+                .WithOne(x => x.Client)
+                .HasForeignKey(x => x.ClientId);
+
+            builder.ToTable("Clients");
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/CommentConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            // Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            // Common Fields
+
+            // CreatedOn
+            builder.Property(x => x.CreatedOn).IsRequired();
+
+            // CreatedByUserId
+            builder.Property(x => x.CreatedByUserId).IsRequired(false);
+            builder.Property(x => x.CreatedByUserId).HasMaxLength(100);
+
+            // ModifiedOn
+            builder.Property(x => x.ModifiedOn).IsRequired(false);
+
+            // ModifiedByUserId
+            builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+            builder.Property(x => x.ModifiedByUserId).HasMaxLength(100);
+
+            // DeletedOn
+            builder.Property(x => x.DeletedOn).IsRequired(false);
+
+            // DeletedByUserId
+            builder.Property(x => x.DeletedByUserId).IsRequired(false);
+            builder.Property(x => x.DeletedByUserId).HasMaxLength(100);
+
+            // IsDeleted
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.IsDeleted).HasDefaultValueSql("0");
+            builder.HasIndex(x => x.IsDeleted);
+
+            // Relationships
+            builder.HasOne<Client>(x => x.Client)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.ClientId);
+
+            builder.HasOne<Lawyer>(x => x.Lawyer)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.LawyerId);
+
+            builder.ToTable("Comments");
+        }
+    }
+}
