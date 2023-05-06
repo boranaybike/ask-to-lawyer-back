03e71141a311918dd24d952748522c97a5926cbf
ik3 <aybikeboran7140@gmail.com>
2023-05-06 18:54:17 +0300

    refactoring

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Domain.Identity;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            var connectionString = configuration.GetConnectionString("MariaDB")!;
+
+            // DbContext
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddDbContext<IdentityContext>(opt => opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            return services;
+        }
+    }
+}
--- WebApi/Controllers/OffersController.cs
+++ WebApi/Controllers/OffersController.cs
@@ -1,14 +1,17 @@
 ﻿using Application.Features.Offers.Commands.Add;
 using Application.Features.Offers.Commands.Delete;
 using Application.Features.Offers.Commands.Update;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class OffersController : ApiControllerBase
+    public class OffersController : ControllerBase
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); 
 
         [HttpPost("Add")]
         public async Task<IActionResult> AddAsync(OfferAddCommand command)
